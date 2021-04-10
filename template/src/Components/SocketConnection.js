8571cfd794e3984aff7539e3503498793cc8dd76
JoshDay127 <Josh@Joshday.uk>
2021-04-10 14:04:22 +0100

    Seperated out client and template

--- template/src/Components/SocketConnection.js
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-
-//const ENDPOINT = `${process.env.REACT_APP_SOCKETIO_SERVER}:${process.env.REACT_APP_SOCKETIO_PORT}`;
-const ENDPOINT = "127.0.0.1:3005";
-//SocketIO connection
-
-const socket = io(ENDPOINT);
-
-export default socket;
